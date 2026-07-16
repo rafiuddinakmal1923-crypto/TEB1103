9e86047dc4886de44c90ecc34f250bd2e17fb648
rafiuddinakmal1923-crypto <rafiuddinakmal1923@gmail.com>
2026-07-16 21:49:41 +0800

    remove comment that somehow cause issue when run in the command prompt

--- PART 3/File1.sql
+++ PART 3/File1.sql
@@ -4,5 +4,5 @@ SELECT
 FROM Job_Applicant A
 JOIN Job_Application B 
     ON A.Applicant_ID = B.Applicant_ID
-WHERE A.Current_Employment_Status = 'Employed'    -- 1st constraint: Text value
-  AND B.Expected_Salary > 1000.00;                -- 2nd constraint: Numeric value
+WHERE A.Current_Employment_Status = 'Employed'    
+  AND B.Expected_Salary > 1000.00;                
--- PART 3/File2.sql
+++ PART 3/File2.sql
@@ -9,7 +9,7 @@ JOIN Proposal_Content P
     ON A.Proposal_ID = P.Proposal_ID
 JOIN Developer D 
     ON P.Developer_ID = D.Developer_ID
-WHERE A.Agreement_GoverningLaw = 'National Land Code (Act 828)' -- Constraint 1
-  AND I.Department_Operation_Budget >= 1000000.00               -- Constraint 2
-  AND D.Developer_Experience_Year > 0                           -- Constraint 3
+WHERE A.Agreement_GoverningLaw = 'National Land Code (Act 828)' 
+  AND I.Department_Operation_Budget >= 1000000.00               
+  AND D.Developer_Experience_Year > 0                           
 GROUP BY I.Department_Name;
